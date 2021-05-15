80825180ffce20cc9653d76733680526897453f8
Xiaochen Xu <xiaochex@gmail.com>
2021-05-15 18:38:00 -0400

    commit tree cf642302df759ebb1bb3bbbc0709df80c77f20ab

--- nested.rb
+++ nested.rb
@@ -15,6 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
+    programmer_hash[:grace_hopper]
 
 end
 
@@ -36,6 +37,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+    programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -56,6 +58,7 @@ def dennis_ritchies_language
       }
     }
 
+    programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -81,6 +84,12 @@ def adding_matz
         :languages => ["C"]
       }
     }
+    programmer_hash[:yukihiro_matsumoto] = [
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+    ]
+
+    programmer_hash
 
 end
 
@@ -104,6 +113,9 @@ def changing_alan
       }
     }
 
+    programmer_hash[:alan_kay][:known_for]= "GUI"
+    programmer_hash
+
 end
 
 def adding_to_dennis
@@ -126,4 +138,6 @@ def adding_to_dennis
       }
     }
 
+    programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+programmer_hash
 end
